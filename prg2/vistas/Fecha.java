1a32add12209ba53c05326a64c64dbdca485b51c
Jose Melgares <rjmzelaya@gmail.com>
2026-02-27 10:44:14 +0100

    adds fecha class public view

---
+++ prg2/vistas/Fecha.java
@@ -0,0 +1,24 @@
+package vistas;
+
+// vista publica de la clase Fecha
+class Fecha {
+    public Fecha(int dia, int mes, int anio) {  }
+
+    // getter q devuelve el valor del dia
+    // a masias no le gustan
+    public int dia;
+    public int Dia() { return dia; }
+
+    public String mostrar() { return ""; }
+
+    // toma la actual instancia de la clase
+    public boolean esBisiesto() { return true; }
+
+    // a una fecha le restamos dias
+    public Fecha restar(int dias) { }
+
+    public void editarFecha(int dia, int mes, int anio)
+    public void editarFecha(Fecha fecha)
+    public void editarFecha(int dia)
+
+}
