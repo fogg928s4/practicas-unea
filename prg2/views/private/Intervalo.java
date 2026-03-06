104dc1a6318817880a5673307d40ea79346608f1
Jose Melgares <rjmzelaya@gmail.com>
2026-03-06 11:53:08 +0100

    implementacion de vista privada clase intervalo

---
+++ prg2/views/private/Intervalo.java
@@ -0,0 +1,94 @@
+class Intervalo {
+
+    // Implementacion privada
+    private double inferior;
+    private double superior;
+    // Otra opcion puede ser
+    // Como es privado no necesita saber como se implementa
+    // private double puntoMedio;
+    // private double longitud;
+
+    public Intervalo (double inferior, double superior) {
+        // longitud = superior - inferior;
+        // puntoMedio = inferior + (longitud /2 );
+        this.inferior = inferior;
+        this.superior = superior;
+    }
+    public Intervalo (double superior) {
+        this(0, superior);
+    }
+    public Intervalo () {
+        this(0,0);
+    }
+    public Intervalo (Intervalo intervalo) {
+        // puede ser
+        // inferior = intervalo.inferior;
+        // superior = intervalo.superior;
+        // o podemos llamar al primer constructor
+        this(intervalo.inferior, intervalo.superior);
+        // es de java pero debe ser la primera linea de la implementacion
+    }
+
+    public Intervalo clone() {
+        return new Intervalo(this.inferior, this.superior);
+    }
+    public double longitud() {
+        return superior - inferior;
+    }
+    public void desplazar(double desplazamiento) {
+        // parte de la vista privada, no se ve
+        //puntoMedio += desplazamiento;
+        superior+=desplazamiento;
+        inferior+=desplazamiento;
+
+    }
+    public Intervalo desplazado(double desplazamiento) {
+        return new Intervalo(inferior+desplazamiento, superior +desplazamiento);
+
+        //o usando desplazar
+        Intervalo temp = this.clone();
+        temp.desplazar(desplazamiento);
+        return temp;
+    }
+
+    public boolean incluye(double punto) {
+        return inferior <= punto && superior >= punto;
+    }
+    public boolean incluye(Intervalo intervalo) {
+        // Objetos de la misma clase pueden interactuar con
+        // atributos privados del mismo tipo
+        return this.superior >= intervalo.superior && 
+                this.inferior <= intervalo.inferior;  
+        
+        // o
+        return this.incluye(intervalo.inferior) && this.incluye(intervalo.superior);
+    }
+    public boolean equals(Intervalo intervalo) {
+        return this.superior == intervalo.superior && 
+                this.inferior == intervalo.inferior;
+    }
+
+    public Intervalo interseccion(Intervalo intervalo) {
+
+    }
+    public boolean intersecta(Intervalo intervalo)   {
+        // reusando tanto como se pueda
+        return this.incluye(intervalo.inferior) 
+                || this.incluye(intervalo.superior)
+                || intervalo.incluye(this);
+    }
+    
+    public void oponer()
+    public void doblar() {
+        double longitud = this.longitud();
+        inferior = inferior-(longitud/2);
+        superior = superior+(longitud/2);
+    }
+    public void mostrar()
+    public void recoger()
+
+    public Intervalo[] trocear(int numeroTrozos)
+    public Intervalo union(Intervalo intervalo)
+    public double puntoMedio();
+
+}
